946fa4137e8598d89c0b9eec678adf950ceb32cc
qq2868708 <44116994+qq2868708@users.noreply.github.com>
2020-12-10 17:08:08 +0800

    Json

--- Assets/Scripts/ARWorldMap/ObjSave.cs
+++ Assets/Scripts/ARWorldMap/ObjSave.cs
@@ -69,14 +69,62 @@ public class ObjSave : MonoBehaviour
 
     public void Save()
     {
-        SaveObj();
+        SaveJson();
     }
 
     public void Load()
     {
-        LoadObj();
+        LoadJson();
     }
 
+    #region Json序列化对象
+    private void SaveJson()
+    {
+        FileStream file = new FileStream(pathJson, FileMode.Create);
+        file.Close();
+        file = new FileStream(pathJson, FileMode.Append);
+        for (int i = 0; i < gameObjectRoot.transform.childCount; i++)
+        {
+            var temp = new GamObjectInfomation();
+            var tempObj = gameObjectRoot.transform.GetChild(i);
+            temp.gamObjectName = tempObj.name;
+            temp.gamObjectName = temp.gamObjectName.IndexOf('(') == -1 ? temp.gamObjectName : temp.gamObjectName.Substring(0, temp.gamObjectName.IndexOf('('));
+            temp.gamObjectName = temp.gamObjectName.IndexOf(' ') == -1 ? temp.gamObjectName : temp.gamObjectName.Substring(0, temp.gamObjectName.IndexOf(' '));
+#if UNITY_EDITOR
+            Debug.Log(temp.gamObjectName);
+#endif
+            temp.gameObjectPosition = tempObj.position;
+            temp.gamObjectRotation = tempObj.rotation;
+            temp.gameObjectScale = tempObj.localScale;
+            var jsonStr = JsonUtility.ToJson(temp)+"\n";
+            file.Write(System.Text.Encoding.Default.GetBytes(jsonStr),0,System.Text.Encoding.Default.GetBytes(jsonStr).Length);
+            file.Flush();
+        }
+        file.Close();
+    }
+
+    private void LoadJson()
+    {
+        var content = File.ReadAllBytes(pathJson);
+        var str = System.Text.Encoding.Default.GetString(content);
+        var objs = str.Split('\n');
+        foreach (var obj in objs)
+        {
+            GamObjectInfomation temp = JsonUtility.FromJson<GamObjectInfomation>(obj);
+            var path = "Model/" + temp.gamObjectName + "/" + temp.gamObjectName;
+#if UNITY_EDITOR
+            Debug.Log(path);
+#endif
+            var tempRes = Resources.Load(path) as GameObject;
+            var tempObj = GameObjectPool.instance.CreateObject(temp.gamObjectName, tempRes, temp.gameObjectPosition, temp.gamObjectRotation);
+            tempObj.transform.position = temp.gameObjectPosition;
+            tempObj.transform.rotation = temp.gamObjectRotation;
+            tempObj.transform.localScale = temp.gameObjectScale;
+        }
+    }
+    #endregion
+
+    #region C#接口序列化对象
     private void SaveObj()
     {
         for (int i = 0; i < gameObjectRoot.transform.childCount; i++)
@@ -116,11 +164,6 @@ public class ObjSave : MonoBehaviour
         }
     }
 
-    public void DestroyObj()
-    {
-        GameObjectPool.instance.ClaerAll();
-    }
-
     void SaveBinary()
     {
         try
@@ -147,7 +190,7 @@ public class ObjSave : MonoBehaviour
     {
         try
         {
-            FileStream file=File.Open(pathObj,FileMode.Open);
+            FileStream file = File.Open(pathObj, FileMode.Open);
             BinaryFormatter bf = new BinaryFormatter();
             SurrogateSelector ss = new SurrogateSelector();
             var streamingContext = new StreamingContext(StreamingContextStates.All);
@@ -164,6 +207,14 @@ public class ObjSave : MonoBehaviour
         }
     }
 
+    #endregion
+
+    public void DestroyObj()
+    {
+        GameObjectPool.instance.ClaerAll();
+    }
+
+
     string pathObj
     {
         get
@@ -172,6 +223,18 @@ public class ObjSave : MonoBehaviour
             return Path.Combine(Application.streamingAssetsPath, "my_session_Obj.worldmap");
 #elif UNITY_IOS
             return Path.Combine(Application.persistentDataPath, "my_session_Obj.worldmap");
+#endif
+        }
+    }
+
+    string pathJson
+    {
+        get
+        {
+#if UNITY_EDITOR
+            return Path.Combine(Application.streamingAssetsPath, "my_session_Json.txt");
+#elif UNITY_IOS
+            return Path.Combine(Application.persistentDataPath, "my_session_Json.txt");
 #endif
         }
     }
